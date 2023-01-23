a849c7300e13fb79bb63bf56898d40a881b78c40
Sora Tonami <ms0503@outlook.com>
2023-01-23 16:20:26 +0900

    LGPLのライセンス告知を各ファイルに追加。

--- include/Can.hpp
+++ include/Can.hpp
@@ -3,6 +3,14 @@
  *
  *  Created on: 2019/06/29
  *      Author: youda
+ *
+ *  This file is part of libmecha.
+ *
+ *  libmecha is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  libmecha is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License along with libmecha. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef MYLIBINC_CAN_HPP_
