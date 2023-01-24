8b870054c782f6ee65ef03af60bfe71b5c094d39
Sora Tonami <ms0503@outlook.com>
2023-01-24 22:54:50 +0900

    ビルドスクリプトを修正。

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(realpath "$(dirname "$0")")"
 mkdir -p build
-cmake -DLIBMECHA_ONLY:BOOL=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi_toolchain.cmake -S . -B build
+cmake -DLIBMECHA_ONLY=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi_toolchain.cmake -S . -B build
 cd build
 make -j$(($(grep cpu.cores /proc/cpuinfo | sort -u | sed 's/[^0-9]//g') + 1))
 exit 0
