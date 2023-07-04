ff490113856190389644c8a77bee1708990b7d99
Sora Tonami <ms0503@outlook.com>
2023-07-04 13:32:39 +0900

    インストールスクリプトの試験的なSTM32CubeIDE対応。

--- install.ps1
+++ install.ps1
@@ -1,46 +1,90 @@
-set-location "$PSScriptRoot"
+Set-Location "$PSScriptRoot"
 [string] $SCRIPT_VERSION = "1.0.2"
-write-host "libmecha インストールスクリプト Ver.$SCRIPT_VERSION" -foregroundcolor cyan
-write-host "Created by M.s [22er]" -foregroundcolor cyan
-write-host
-if(((test-path "..\CMakeLists.txt") -eq "False") -or ((test-path "..\CMakeLists_template.txt") -eq "False")) {
-    write-host "Error: 親ディレクトリがSTM32プロジェクト(CMakeプロジェクト)ではありません。" -foregroundcolor red
-    write-host "       STM32プロジェクトルートの直下にlibmechaディレクトリ、その配下にこのファイルが存在するようにして下さい。" -foregroundcolor red
-    exit 1
+[bool] $DETECT_ECLIPSE = $false
+[bool] $DETECT_IDEA = $false
+
+function _error {
+    Write-Host "[ERROR] $args" -ForegroundColor Red
 }
-if((((get-acl "..\CMakeLists.txt").Access | where-object {$_.IdentityReference -match "$User" -and $_.AccessControlType -match "Allow"} | foreach-object {$t = $False} {$t = (($_FileSystemRights -band [System.Security.AccessControl.FileSystemRights]::Read) -eq [System.Security.AccessControl.FileSystemRights]::Read) -or $t} {$t}) -eq "False") -or (((get-acl "..\CMakeLists_template.txt").Access | where-object {$_.IdentityReference -match "$User" -and $_.AccessControlType -match "Allow"} | foreach-object {$t = $False} {$t = (($_FileSystemRights -band [System.Security.AccessControl.FileSystemRights]::Read) -eq [System.Security.AccessControl.FileSystemRights]::Read) -or $t} {$t}) -eq "False")) {
-    write-host "Error: ルートプロジェクトのCMakeLists.txtに読み取り権限がありません。" -foregroundcolor red
-    write-host "       権限を付与するか適切なユーザーで実行して下さい。" -foregroundcolor red
-    exit 2
+
+function _info {
+    Write-Host "[INFO]  $args" -ForegroundColor Green
 }
-if((((get-acl "..\CMakeLists.txt").Access | where-object {$_.IdentityReference -match "$User" -and $_.AccessControlType -match "Allow"} | foreach-object {$t = $False} {$t = (($_FileSystemRights -band [System.Security.AccessControl.FileSystemRights]::Write) -eq [System.Security.AccessControl.FileSystemRights]::Write) -or $t} {$t}) -eq "False") -or (((get-acl "..\CMakeLists_template.txt").Access | where-object {$_.IdentityReference -match "$User" -and $_.AccessControlType -match "Allow"} | foreach-object {$t = $False} {$t = (($_FileSystemRights -band [System.Security.AccessControl.FileSystemRights]::Write) -eq [System.Security.AccessControl.FileSystemRights]::Write) -or $t} {$t}) -eq "False")) {
-    write-host "Error: ルートプロジェクトのCMakeLists.txtに書き込み権限がありません。" -foregroundcolor red
-    write-host "       権限を付与するか適切なユーザーで実行して下さい。" -foregroundcolor red
-    exit 3
+
+function _getPerm([string] $name, [System.Security.AccessControl.FileSystemRights] $perm) {
+    return ((Get-Acl "$name").Access | Where-Object {$_.IdentityReference -match "$User" -and $_.AccessControlType -match "Allow"} | ForEach-Object {$t = $False} {$t = (($_FileSystemRights -band $perm) -eq $perm) -or $t} {$t}) -eq "False"
 }
-if(((get-content "..\CMakeLists.txt" | select-string -quiet "# Powered by libmecha from mechalab")) -or ((get-content "..\CMakeLists_template.txt" | select-string -quiet "# Powered by libmecha from mechalab"))) {
-    write-host "Info: 既に組み込まれています。" -foregroundcolor green
-    exit 0
+
+Write-Host "libmecha インストールスクリプト Ver.$SCRIPT_VERSION" -ForegroundColor Cyan
+Write-Host "Created by M.s [22er]" -ForegroundColor Cyan
+Write-Host
+_info "環境を検出中..."
+if((Test-Path "..\.cproject") -and (Test-Path "..\.mxproject") -and (Test-Path "..\.project")) {
+    $DETECT_ECLIPSE = $true
 }
-write-host "Info: ルートプロジェクトにlibmechaを組み込みます。" -foregroundcolor green
-$result = $host.ui.PromptForChoice("", "よろしいですか？", [System.Management.Automation.Host.ChoiceDescription[]]((new-object System.Management.Automation.Host.ChoiceDescription "&y" "インストールを実行する。"), (new-object System.Management.Automation.Host.ChoiceDescription "&n" "インストールを中断する。")), 0)
+if(Test-Path "..\.idea") {
+    $DETECT_IDEA = $true;
+}
+if($DETECT_IDEA) {
+    if(!(Test-Path "..\CMakeLists.txt") -or !(Test-Path "..\CMakeLists_template.txt")) {
+        _error "親ディレクトリがSTM32プロジェクト(CMakeプロジェクト)ではありません。"
+        _error "STM32プロジェクトルートの直下にlibmechaディレクトリ、その配下にこのファイルが存在するようにして下さい。"
+        exit 2
+    }
+    if((_getPerm "..\CMakeLists.txt" [System.Security.AccessControl.FileSystemRights]::Read) -and (_getPerm "..\CMakeLists_template.txt" [System.Security.AccessControl.FileSystemRights]::Read)) {
+        _error "ルートプロジェクトのCMakeLists.txtに読み取り権限がありません。"
+        _error "権限を付与するか適切なユーザーで実行して下さい。"
+        exit 3
+    }
+    if((_getPerm "..\CMakeLists.txt" [System.Security.AccessControl.FileSystemRights]::Write) -and (_getPerm "..\CMakeLists_template.txt" [System.Security.AccessControl.FileSystemRights]::Write)) {
+        _error "ルートプロジェクトのCMakeLists.txtに書き込み権限がありません。"
+        _error "権限を付与するか適切なユーザーで実行して下さい。"
+        exit 4
+    }
+    if((Get-Content "..\CMakeLists.txt" | Select-String -Quiet "# Powered by libmecha from mechalab") -or (Get-Content "..\CMakeLists_template.txt" | Select-String -Quiet "# Powered by libmecha from mechalab")) {
+        _info "既に組み込まれています。"
+        exit 5
+    }
+} elseif($DETECT_ECLIPSE) {
+    if((!(Test-Path "..\Core") -or !(Test-Path "..\Core\Inc") -or !(Test-Path "..\Core\Src")) -and (!(Test-Path "..\Inc") -or !(Test-Path "..\Src"))) {
+        _error "ルートプロジェクトのインクルード・ソースディレクトリが見つかりません。"
+        exit 6
+    }
+    if((!(_getPerm "..\Core" [System.Security.AccessControl.FileSystemRights]::Write) -or !(_getPerm "..\Core\Inc" [System.Security.AccessControl.FileSystemRights]::Write) -or !(_getPerm "..\Core\Src" [System.Security.AccessControl.FileSystemRights]::Write)) -and (!(_getPerm "..\Inc" [System.Security.AccessControl.FileSystemRights]::Write) -or !(_getPerm "..\Src" [System.Security.AccessControl.FileSystemRights]::Write))) {
+        _error "ルートプロジェクトのインクルード・ソースディレクトリに書き込み権限がありません。"
+        _error "権限を付与するか適切なユーザーで実行して下さい。"
+        exit 7
+    }
+}
+_info "ルートプロジェクトにlibmechaを組み込みます。"
+$result = $Host.UI.PromptForChoice("", "よろしいですか？", [System.Management.Automation.Host.ChoiceDescription[]]((New-Object System.Management.Automation.Host.ChoiceDescription "&y" "インストールを実行する。"), (New-Object System.Management.Automation.Host.ChoiceDescription "&n" "インストールを中断する。")), 0)
 if($result -eq 1) {
-    write-host "Info: 中断しました。" -foregroundcolor green
-    exit 0
+    Write-Host "Info: 中断しました。" -ForegroundColor Green
+    exit 8
 }
-write-output @"
+if($DETECT_IDEA) {
+    Write-Output @"
 
 # Powered by libmecha from mechalab
-include_directories(libmecha/include)
+target_include_directories(`${PROJECT_NAME}.elf PRIVATE libmecha/include)
 add_subdirectory(libmecha)
-target_link_libraries(`${PROJECT_NAME}.elf mecha)
-"@ | out-file -append "..\CMakeLists.txt"
-write-output @"
+target_link_libraries(`${PROJECT_NAME}.elf PRIVATE mecha)
+"@ | Out-File -Append "..\CMakeLists.txt"
+    Write-Output @"
 
 # Powered by libmecha from mechalab
-include_directories(libmecha/include)
+target_include_directories(`$`${PROJECT_NAME}.elf PRIVATE libmecha/include)
 add_subdirectory(libmecha)
-target_link_libraries(`$`${PROJECT_NAME}.elf mecha)
-"@ | out-file -append "..\CMakeLists_template.txt"
-write-host "Info: 正常に終了しました。" -foreground green
+target_link_libraries(`$`${PROJECT_NAME}.elf PRIVATE mecha)
+"@ | Out-File -Append "..\CMakeLists_template.txt"
+} elseif($DETECT_ECLIPSE) {
+    if(Test-Path "..\Core") {
+        Copy-Item -Recurse ".\include\*" -Destination "..\Core\Inc"
+        Copy-Item -Recurse ".\src\*" -Destination "..\Core\Src"
+    } else {
+        Copy-Item -Recurse ".\include\*" -Destination "..\Inc"
+        Copy-Item -Recurse ".\src\*" -Destination "..\Src"
+    }
+}
+_info "正常に終了しました。"
 exit 0
--- install.sh
+++ install.sh
@@ -1,47 +1,84 @@
 #!/bin/bash
+# shellcheck disable=SC2164
 cd "$(realpath "$(dirname "$0")")"
-SCRIPT_VERSION="1.0.2"
-printf "[1;36mlibmecha インストールスクリプト Ver.%s[0m\n" "$SCRIPT_VERSION"
+SCRIPT_VERSION="1.1.0"
+DETECT_ECLIPSE=false
+DETECT_IDEA=false
+
+_error() {
+    printf "[1;31m[ERROR] %s[m\n" "$1"
+}
+
+_info() {
+    printf "[32m[INFO]  %s[m\n" "$1"
+}
+
+printf "[1;36mlibmecha インストールスクリプト Ver.%s[m\n" "$SCRIPT_VERSION"
 printf "[1;36mCreated by M.S [22er][0m\n"
 printf "\n"
-if [[ ! -f ../CMakeLists.txt || ! -f ../CMakeLists_template.txt ]]; then
-    printf "[1;31mError: 親ディレクトリがSTM32プロジェクト(CMakeプロジェクト)ではありません。[0m\n"
-    printf "[1;31m       STM32プロジェクトルートの直下にlibmechaディレクトリ、その配下にこのファイルが存在するようにして下さい。[0m\n"
-    exit 1
+_info "環境を検出中..."
+[[ -f ../.cproject && -f ../.mxproject && -f ../.project ]] && DETECT_ECLIPSE=true
+[[ -d ../.idea ]] && DETECT_IDEA=true
+if DETECT_IDEA; then
+    if [[ ! -f ../CMakeLists.txt || ! -f ../CMakeLists_template.txt ]]; then
+        _error "親ディレクトリがSTM32プロジェクト(CMakeプロジェクト)ではありません。"
+        _error "STM32プロジェクトルートの直下にlibmechaディレクトリ、その配下にこのファイルが存在するようにして下さい。"
+        exit 2
+    fi
+    if [[ ! -r ../CMakeLists.txt || ! -r ../CMakeLists_template.txt ]]; then
+        _error "ルートプロジェクトのCMakeLists.txtに読み取り権限がありません。"
+        _error "権限を付与するか適切なユーザーで実行して下さい。"
+        exit 3
+    fi
+    if [[ ! -w ../CMakeLists.txt || ! -w ../CMakeLists_template.txt ]]; then
+        _error "ルートプロジェクトのCMakeLists.txtに書き込み権限がありません。"
+        _error "権限を付与するか適切なユーザーで実行して下さい。"
+        exit 4
+    fi
+    if grep '# Powered by libmecha from mechalab' ../CMakeLists.txt &>/dev/null || grep '# Powered by libmecha from mechalab' ../CMakeLists_template.txt &>/dev/null; then
+        _info "既に組み込まれています。"
+        exit 5
+    fi
+elif DETECT_ECLIPSE; then
+    if [[ (! -d ../Core || ! -d ../Core/Inc || ! -d ../Core/Src) && (! -d ../Inc || ! -d ../Src) ]]; then
+        _error "ルートプロジェクトのインクルード・ソースディレクトリが見つかりません。"
+        exit 6
+    fi
+    if [[ (! -w ../Core || ! -w ../Core/Inc || ! -w ../Core/Src) && (! -w ../Inc || ! -w ../Src) ]]; then
+        _error "ルートプロジェクトのインクルード・ソースディレクトリに書き込み権限がありません。"
+        _error "権限を付与するか適切なユーザーで実行して下さい。"
+        exit 7
+    fi
 fi
-if [[ ! -r ../CMakeLists.txt || ! -r ../CMakeLists_template.txt ]]; then
-    printf "[1;31mError: ルートプロジェクトのCMakeLists.txtに読み取り権限がありません。[0m\n"
-    printf "[1;31m       権限を付与するか適切なユーザーで実行して下さい。[0m\n"
-    exit 2
-fi
-if [[ ! -w ../CMakeLists.txt || ! -w ../CMakeLists_template.txt ]]; then
-    printf "[1;31mError: ルートプロジェクトのCMakeLists.txtに書き込み権限がありません。[0m\n"
-    printf "[1;31m       権限を付与するか適切なユーザーで実行して下さい。[0m\n"
-    exit 3
-fi
-if grep '# Powered by libmecha from mechalab' ../CMakeLists.txt &>/dev/null || grep '# Powered by libmecha from mechalab' ../CMakeLists_template.txt &>/dev/null; then
-    printf "[32mInfo: 既に組み込まれています。[0m\n"
-    exit 0
-fi
-printf "[32mInfo: ルートプロジェクトにlibmechaを組み込みます。[0m\n"
-read -n1 -p "よろしいですか？ [Y/n]> " result
+_info "ルートプロジェクトにlibmechaを組み込みます。"
+read -r -n1 -p "よろしいですか？ [Y/n]> " result
 if [[ "$result" = [Nn] ]]; then
-    printf "[32mInfo: 中断しました。[0m\n"
-    exit 0
+    _info "中断しました。"
+    exit 8
 fi
-cat <<EOT >> ../CMakeLists.txt
+if DETECT_IDEA; then
+    cat <<EOA >> ../CMakeLists.txt
 
 # Powered by libmecha from mechalab
-include_directories(libmecha/include)
+target_include_directories(\${PROJECT_NAME}.elf PRIVATE libmecha/include)
 add_subdirectory(libmecha)
-target_link_libraries(\${PROJECT_NAME}.elf mecha)
-EOT
-cat <<EOT >> ../CMakeLists_template.txt
+target_link_libraries(\${PROJECT_NAME}.elf PRIVATE mecha)
+EOA
+    cat <<EOA >> ../CMakeLists_template.txt
 
 # Powered by libmecha from mechalab
-include_directories(libmecha/include)
+target_include_directories(\$\${PROJECT_NAME}.elf PRIVATE libmecha/include)
 add_subdirectory(libmecha)
-target_link_libraries(\$\${PROJECT_NAME}.elf mecha)
-EOT
-printf "[32mInfo: 正常に終了しました。[0m\n"
+target_link_libraries(\$\${PROJECT_NAME}.elf PRIVATE mecha)
+EOA
+elif DETECT_ECLIPSE; then
+    if [[ -d ../Core ]]; then
+        cp -arv ./include/* ../Core/Inc
+        cp -arv ./src/* ../Core/Src
+    else
+        cp -arv ./include/* ../Inc
+        cp -arv ./src/* ../Src
+    fi
+fi
+_info "正常に終了しました。"
 exit 0
