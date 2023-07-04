e041786d99e8a5b68dcaee764f1cf3307a700ad7
Sora Tonami <ms0503@outlook.com>
2023-07-04 17:13:44 +0900

    ビルドスクリプトの削除。 不要となったツールチェインファイルの削除。

--- build.ps1
+++
@@ -1,6 +0,0 @@
-set-location "$PSScriptRoot"
-new-item -path . -name build -itemtype directory -force
-cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi_toolchain.cmake -S . -B build
-set-location build
-msbuild .\libmecha.sln
-
--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# shellcheck disable=SC2164
-cd "$(realpath "$(dirname "$0")")"
-mkdir -p build
-cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=arm-none-eabi_toolchain.cmake -S . -B build
-cd build
-make -j$(($(grep cpu.cores /proc/cpuinfo | sort -u | sed 's/[^0-9]//g') + 1))
-exit 0
-
